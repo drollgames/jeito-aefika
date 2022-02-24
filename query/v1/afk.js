381302d7f434ab6cd6e0d9fbc786f86c6cfbd02e
__p r i v a t e !! <73804955+drollgames@users.noreply.github.com>
2022-02-24 00:40:38 -0300

    Add files via upload

---
+++ query/v1/afk.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let hour = 0;
+let minute = 0;
+let second = 0;
+let millisecond = 0;
+
+let cron;
+
+document.form_main.start.onclick = () => start();
+document.form_main.pause.onclick = () => pause();
+document.form_main.reset.onclick = () => reset();
+
+function start() {
+  pause();
+  cron = setInterval(() => { timer(); }, 10);
+}
+
+function pause() {
+  clearInterval(cron);
+}
+
+function reset() {
+  hour = 0;
+  minute = 0;
+  second = 0;
+  millisecond = 0;
+  document.getElementById('hour').innerText = '00';
+  document.getElementById('minute').innerText = '00';
